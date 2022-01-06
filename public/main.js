9b5b97244e10c0cb7fab5c1f5637585b6c51e130
wau2380 <wau2380@gmail.com>
2022-01-06 21:36:07 +0900

    hhh

---
+++ public/main.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCrn9qL0YPLDSyeNob4ldA2hw-TaFKz0IQ",
+  authDomain: "solbitzae.firebaseapp.com",
+  databaseURL: "https://solbitzae-default-rtdb.firebaseio.com",
+  projectId: "solbitzae",
+  storageBucket: "solbitzae.appspot.com",
+  messagingSenderId: "63026107814",
+  appId: "1:63026107814:web:5867907b370d55e76eeb67",
+  measurementId: "G-TWB9LCC1DM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
