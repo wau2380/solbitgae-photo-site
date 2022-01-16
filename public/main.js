6979bdbe1c340a29e07a5353bacb72f5a26a14c7
wau2380 <wau2380@gmail.com>
2022-01-16 22:33:47 +0900

    dd

--- public/main.js
+++ public/main.js
@@ -1,12 +1,6 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCrn9qL0YPLDSyeNob4ldA2hw-TaFKz0IQ",
   authDomain: "solbitzae.firebaseapp.com",
@@ -18,6 +12,84 @@ const firebaseConfig = {
   measurementId: "G-TWB9LCC1DM",
 };
 
+import {
+  collection,
+  getFirestore,
+  addDoc,
+  serverTimestamp,
+} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var timestamp = Number(new Date());
+  var storageRef = firebase.storage().ref(timestamp.toString());
+  var $ = jQuery;
+
+  var file_data = $("#uploadZip").prop("files")[0];
+  console.log(file_data);
+  storageRef.put(file_data);
+});
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var timestamp = Number(new Date());
+  var storageRef = firebase.storage().ref(timestamp.toString());
+  var $ = jQuery;
+
+  var file_data = $("#uploadZip2").prop("files")[0];
+  console.log(file_data);
+  storageRef.put(file_data);
+});
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var timestamp = Number(new Date());
+  var storageRef = firebase.storage().ref(timestamp.toString());
+  var $ = jQuery;
+
+  var file_data = $("#uploadZip3").prop("files")[0];
+  console.log(file_data);
+  storageRef.put(file_data);
+});
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var timestamp = Number(new Date());
+  var storageRef = firebase.storage().ref(timestamp.toString());
+  var $ = jQuery;
+
+  var file_data = $("#uploadZip4").prop("files")[0];
+  console.log(file_data);
+  storageRef.put(file_data);
+});
+
+const db = getFirestore();
+// // const auth = getAuth();
+
+// // collection ref
+const colRef = collection(db, "classes");
+
+const addStudnetsForm = document.querySelector("form");
+addStudnetsForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    class: addStudnetsForm.class.value,
+
+    createdAt: serverTimestamp(),
+  }).then(() => {
+    addStudnetsForm.reset();
+    console.log();
+  });
+});
